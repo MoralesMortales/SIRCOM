11b2fec4b239b321164aea46a279b4651b8fdac2
MoralesMortales <carlos52.7624@gmail.com>
2025-08-31 14:39:59 -0400

    loginWindow

--- app/main.py
+++ app/main.py
@@ -1,13 +1,13 @@
 import sys
 from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton
 
-class MiVentana(QMainWindow):
+class LoginWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.inicializar_ui()
     
     def inicializar_ui(self):
-        self.setWindowTitle("Mi Primera App PyQt")
+        self.setWindowTitle("Sistema de Almacen")
         self.setGeometry(100, 100, 400, 300)
         
         etiqueta = QLabel("¡Hola PyQt!", self)
@@ -16,13 +16,14 @@ class MiVentana(QMainWindow):
         boton = QPushButton("Haz clic", self)
         boton.move(150, 150)
         boton.clicked.connect(self.on_button_click)
+
+        self.showMaximized()
     
     def on_button_click(self):
         print("¡Botón clickeado!")
 
-# Punto de entrada de la aplicación
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    ventana = MiVentana()
+    ventana = LoginWindow()
     ventana.show()
     sys.exit(app.exec())
