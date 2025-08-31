9c4507a45c14320adcd3399f02818904842ebd8a
MoralesMortales <carlos52.7624@gmail.com>
2025-08-31 15:33:40 -0400

    loginDone(window)

--- app/main.py
+++ app/main.py
@@ -1,29 +1,9 @@
 import sys
 from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton
-
-class LoginWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.inicializar_ui()
-    
-    def inicializar_ui(self):
-        self.setWindowTitle("Sistema de Almacen")
-        self.setGeometry(100, 100, 400, 300)
-        
-        etiqueta = QLabel("¡Hola PyQt!", self)
-        etiqueta.move(150, 100)
-        
-        boton = QPushButton("Haz clic", self)
-        boton.move(150, 150)
-        boton.clicked.connect(self.on_button_click)
-
-        self.showMaximized()
-    
-    def on_button_click(self):
-        print("¡Botón clickeado!")
+from views.auth.login import LoginView 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    ventana = LoginWindow()
+    ventana = LoginView()
     ventana.show()
     sys.exit(app.exec())
---
+++ app/views/auth/login.py
@@ -0,0 +1,10 @@
+from PyQt6.QtWidgets import QWidget
+
+from windows.py.loginWindow import Ui_container  
+
+class LoginView(QWidget, Ui_container):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.setWindowTitle("Historial")
+
