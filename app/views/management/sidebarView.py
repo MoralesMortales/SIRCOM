cda0fba9ccb2ad745e36921525e1d4c7137e7628
MoralesMortales <carlos52.7624@gmail.com>
2025-10-05 11:59:02 -0400

    btnRecognizable

--- app/views/management/sidebarView.py
+++ app/views/management/sidebarView.py
@@ -1,3 +1,5 @@
+from PyQt5 import QtGui
+from PyQt5 import QtCore
 from PyQt5.QtWidgets import QWidget, QApplication
 from app.windows.py.sidebarWds import Ui_Form
 from PyQt5.QtCore import QEvent, Qt, QTimer
@@ -7,6 +9,7 @@ class sidebarView(QWidget, Ui_Form):
         super().__init__(parent)
         self.setupUi(self)
         self.setup_sidebar()
+        self.make_labels_clickable()
         
     def setup_sidebar(self):
         self.setWindowFlags(Qt.FramelessWindowHint | Qt.Tool)
@@ -19,7 +22,6 @@ class sidebarView(QWidget, Ui_Form):
         
         self.show()
         
-        # Usar un timer para instalar el event filter después de mostrar
         QTimer.singleShot(100, self.install_event_filter)
         
     def install_event_filter(self):
@@ -27,7 +29,6 @@ class sidebarView(QWidget, Ui_Form):
         
     def eventFilter(self, obj, event):
         if event.type() == QEvent.MouseButtonPress:
-            # Verificar si el clic fue fuera de este widget
             click_pos = event.globalPos()
             if not self.rect().contains(self.mapFromGlobal(click_pos)):
                 self.hide()
@@ -37,3 +38,41 @@ class sidebarView(QWidget, Ui_Form):
     def hideEvent(self, event):
         QApplication.instance().removeEventFilter(self)
         super().hideEvent(event)
+
+    def make_labels_clickable(self):
+        """Hacer que los labels sean clickeables y conectar sus señales"""
+        # Lista de labels que queremos hacer clickeables
+        clickable_labels = [
+            self.labelInventario,
+            self.labelNuevo,
+            self.labelHistorial,
+            self.labelEstadistica,
+            self.label_13  # También el label "Salir"
+        ]
+        
+        # Configurar cada label como clickeable
+        for label in clickable_labels:
+            label.setProperty("clickable", "true")
+            label.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
+            label.mousePressEvent = self.create_click_handler(label.objectName())
+
+    def create_click_handler(self, label_name):
+        """Crear un manejador de eventos para cada label"""
+        def click_handler(event):
+            print("hola")  # Esto se ejecutará en cada clic
+            # También puedes imprimir el nombre del label que fue clickeado
+            print(f"Label clickeado: {label_name}")
+            
+            # Aquí puedes agregar lógica específica para cada label si lo deseas
+            if label_name == "labelInventario":
+                print("Acción: Inventario")
+            elif label_name == "labelNuevo":
+                print("Acción: Nuevo producto")
+            elif label_name == "labelHistorial":
+                print("Acción: Historial")
+            elif label_name == "labelEstadistica":
+                print("Acción: Estadísticas")
+            elif label_name == "label_13":
+                print("Acción: Salir")
+                
+        return click_handler
